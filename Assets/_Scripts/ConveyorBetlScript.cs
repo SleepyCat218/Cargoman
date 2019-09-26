ad72868e8cd10acfc4081428a2d6be6d1caf1083
SleepyCat218 <sleepycat21827@gmail.com>
2019-09-26 13:33:35 +0300

    done: conveyor belt moving

---
+++ Assets/_Scripts/ConveyorBetlScript.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+public class ConveyorBetlScript : MonoBehaviour
+{
+    private Material mat;
+    [SerializeField] private float _beltSpeed = 20.1f;
+    
+    private void Awake()
+    {
+        mat = gameObject.GetComponent<Renderer>().material;
+        mat.SetFloat("_ScrollYSpeed", _beltSpeed);
+    }
+
+}
